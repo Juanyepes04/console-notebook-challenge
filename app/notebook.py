6a1700d2b5801e9d1724e83d5e1cebc816653ab0
Juan Diego Yepes <jyepes023@soyudemedellin.edu.co>
2025-02-18 12:56:21 -0500

    note class and first method

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,18 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+from datetime import datetime
+
+
+class Note:
+    HIGH = 'HIGH'
+    MEDIUM = 'MEDIUM'
+    LOW = 'LOW'
+
+    def __init__(self, code: int, title: str, text: str, importance: str):
+        self.code = code
+        self.title = title
+        self.text = text
+        self.importance = importance
+        self.creation_date = datetime.now()
+        self.tags = []
+
+
